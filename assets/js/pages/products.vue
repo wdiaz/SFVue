27f31822bed7d511b3004d2dabc5a818859cc278
Walter Diaz <wdiaz@cs.columbia.edu>
2020-10-28 16:36:15 -0400

    quick tweak

---
+++ assets/js/pages/products.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Hello {{ firstName }}, welcome back! </h1>
+</template>
+
+<script>
+export default {
+    name: 'Products',
+    data() {
+        return {
+            firstName: 'Walter Diaz',
+        };
+    },
+};
+</script>
--- assets/js/products.js
+++ assets/js/products.js
@@ -1,14 +1,8 @@
 import Vue from 'vue';
+import App from './pages/products';
 
 new Vue({
-    el: '#app',
-    data() {
-        return {
-            firstName: 'Walter Diaz',
-        };
-    },
-    template: '<h1>Hello {{firstName }}, welcome!</h1>',
     render(h) {
-        return Vue.compile(this.$options.template).render.call(this, h);
+        return h(App);
     },
-});
+}).$mount('#app');
